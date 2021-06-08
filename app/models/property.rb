3fc19ebb0d70c0b0814620cfb4d01b762a38cdda
Charles <charlescampet03@gmail.com>
2021-06-08 22:20:43 +0200

    remove attached

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,11 +1,8 @@
 class Property < ApplicationRecord
     belongs_to :user
 
-	has_many_attached :images
-
     validates :name, presence: true
     validates :description, presence: true
     validates :price, presence: true
 
-	validates :images, attached: true, limit: { min: 1, max: 3 }, content_type: [:png, :jpg, :jpeg]
 end
